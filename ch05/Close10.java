67d2e37394e72a3de5af153f0058e4349c9240ab
Justin Neil <justinmneil@gmail.com>
2016-06-28 20:34:26 -0400

    Moved Close10 and added files for poll exercise

--- ch05/Close10.java
+++ ch05/Close10.java
@@ -1,18 +1,27 @@
 package ch05;
 
+/**
+ * Created by justin on 6/28/16.
+ */
 public class Close10 {
 
-  public static int close10(int a, int b)
-  {
-      int adiff = Math.abs(a - 10);
-      int bdiff = Math.abs(b - 10);
+    public static String close10(int a, int b)
+    {
+        int aDiff = Math.abs(a - 10);
+        int bDiff = Math.abs(b - 10);
 
-      if (adiff == bdiff) return 0;
-      if (adiff > bdiff) return a;
-      return b;
-  }
+        String result = "Comparing " + a + " and " + b + ": ";
 
-  public static void main(String[] args) {
-      System.out.println(close10(12, 18));
-  }
+        if (aDiff == bDiff) result += "Tie!";
+        else if (aDiff < bDiff) result += a + " is closer.";
+        else result += b + " is closer.";
+        return result;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(close10(5, 11));
+        System.out.println(close10(12, 19));
+        System.out.println(close10(6, 1));
+        System.out.println(close10(5, 15));
+    }
 }
