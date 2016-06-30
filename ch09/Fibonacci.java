88d316ba448a83806e20f58b34d96ab96d137268
Justin Neil <justinmneil@gmail.com>
2016-06-30 08:21:19 -0400

    Refactored Fibonacci to use recursive function

--- ch09/Fibonacci.java
+++ ch09/Fibonacci.java
@@ -8,26 +8,32 @@ public class Fibonacci
 
   public static void main(String[] args)
   {
-    int[] fib6 = fibonacci(6);
+    int[] fibNums = fibArray(20);
 
-    for(int i : fib6)
+    for(int i : fibNums)
       System.out.print(i + "  ");
   }
 
-  public static int[] fibonacci(int n)
+  public static int[] fibArray(int n)
   {
     int a = 0, b = 1, temp;
     int[] result = new int[n+1];
 
     for(int i = 0; i <= n; i++)
     {
-      result[i] = a;
-      temp = a + b;
-      a = b;
-      b = temp;
+      result[i] = fibRecursive(i);
+      //temp = a + b;
+      //a = b;
+      //b = temp;
     }
 
     return result;
+  }
+
+  public static int fibRecursive(int n)
+  {
+    if ( n < 2 ) return n;
+    else return (fibRecursive(n-1) + fibRecursive(n-2));
   }
 
 }
